b8982f6a89c7b23ccc6422cff96cee97da31e89b
KWAVE\kwave <kwave55@gmail.com>
2017-06-01 14:43:10 +0900

    Check Permisson ok

--- app/src/main/java/com/android/kwave/contacts/MainActivity.java
+++ app/src/main/java/com/android/kwave/contacts/MainActivity.java
@@ -1,13 +1,31 @@
 package com.android.kwave.contacts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnContact).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), CheckPermissionActivity.class);
+                                        // getBaseContext()는 자신을 가리키는 기능을 함
+                                        // 여기서는 MainActivity.this와 같은 역할
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
